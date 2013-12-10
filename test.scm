a485784f1081a2bdd52723aa4e000e8543357712
bobuhiro11 <blackcherry.mn@gmail.com>
2013-12-10 14:32:54 +0900

    fix test.scm

--- test.scm
+++ test.scm
@@ -53,3 +53,36 @@
       (gcd2 y (modulo x y)))))
 
 (gcd2 30 9)
+
+;
+; ソート
+;
+
+; (large 5 (quote (2 5 9))) => (5 9)
+(define large
+  (lambda (x y)
+    (if (nil? y)
+      nil
+      (if (>= (car y) x)
+        (cons (car y) (large x (cdr y)))
+        (large x (cdr y))))))
+(large 5 (quote (2 5 9)))
+
+; (small 5 (quote (2 5 9))) => (2)
+(define small
+  (lambda (x y)
+    (if (nil? y)
+      nil
+      (if (< (car y) x)
+        (cons (car y) (small x (cdr y)))
+        (small x (cdr y))))))
+(small 5 (quote (2 5 9)))
+
+; (quicksort (quote (3 4 6 4 ))) => (3 4 6 4)
+; (define quicksort
+;   (lambda (x)
+;     (if (nil? x)
+;       nil
+;       (append
+;         (quicksort (large (car x) x))
+;         (quicksort (small (car x) x))))))
